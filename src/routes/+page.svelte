caebcb56d1469f63fbb7ee1ea590c3c5e7e30ac6
Tadeas Pala <tadapala@seznam.cz>
2023-01-14 16:10:28 +0100

    implement keyboard controls

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,13 @@
     import type { Station } from "src/interfaces/Station";
     import { fly } from "svelte/transition";
     import FaSearch from "svelte-icons/fa/FaSearch.svelte";
+    import { goto } from "$app/navigation";
 
     let stationName: string = "";
+    let selected: number = 0;
     let stations: Station[] = [];
+    let inputEl: HTMLInputElement;
+
     async function getStations() {
         const res = await fetch(
             "/api/station?" + new URLSearchParams({ stationName, limit: "20" }),
@@ -15,7 +19,23 @@
         const data = await res.json();
         stations = data.stations;
     }
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+        if (event.key === "ArrowDown") {
+            selected = (selected + 1) % stations.length;
+        } else if (event.key === "ArrowUp") {
+            if (selected === 0) selected = stations.length - 1;
+            else selected = selected - 1;
+        } else if (["f", "/"].includes(event.key) && !event.ctrlKey) {
+            event.preventDefault();
+            inputEl.focus();
+        } else if (event.key === "Enter" && stations.length > 0) {
+            goto(`/board/${stations[selected].node}`);
+        }
+    };
 </script>
+
+<svelte:body on:keydown={handleKeyDown} />
 
 <main class="py-10 px-6 flex flex-col items-center">
     <div class="w-full md:w-4/5">
@@ -26,6 +46,7 @@
             <input
                 bind:value={stationName}
                 on:input={getStations}
+                bind:this={inputEl}
                 placeholder="Název stanice..."
                 class="bg-transparent w-full text-xl md:text-4xl font-semibold outline-none text-gray-300 placeholder:text-gray-600"
             />
@@ -34,12 +55,12 @@
             {#if stationName}
                 {#key stations}
                     <ul in:fly={{ y: -5, duration: 400 }}>
-                        {#each stations as station}
-                            <li
-                                class="hover:bg-gray-800 text-gray-300  border-b-2 border-gray-700 hover:underline"
-                            >
+                        {#each stations as station, i}
+                            <li class="border-b-2 border-gray-700">
                                 <a
-                                    class="md:p-4 p-3 w-full h-full inline-block"
+                                    class="text-gray-300 md:p-4 p-3 w-full hover:bg-gray-800 focus:underline h-full inline-block"
+                                    class:bg-gray-800={i === selected}
+                                    class:underline={i === selected}
                                     href="/board/{station.node}"
                                 >
                                     <h1 class="md:text-xl">{station.fullName}</h1>
