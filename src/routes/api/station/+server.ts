564ce07ee13b5510b7d8a73d46c20ff162478973
Tadeas Pala <tadapala@seznam.cz>
2023-01-14 16:18:46 +0100

    improve station name preprocessing

--- src/routes/api/station/+server.ts
+++ src/routes/api/station/+server.ts
@@ -6,6 +6,15 @@ import data from "../../../stops.json";
 const DEFAULT_LIMIT = 10;
 const DEFAULT_SKIP = 0;
 
+const prepName = (stationName: string): string => {
+    return stationName
+        .replace(/[\W_]+/g, " ") // replace nonaplhanumeric
+        .replace(/\s\s+/g, " ") // replace multiple whitespace chars
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/\p{Diacritic}/gu, "");
+};
+
 export const GET: RequestHandler = async ({ url }) => {
     const stationName = url.searchParams.get("stationName");
     if (stationName === null) {
@@ -17,16 +26,11 @@ export const GET: RequestHandler = async ({ url }) => {
     const limitStr = url.searchParams.get("limit");
     const skip = skipStr ? Number(skipStr) : DEFAULT_SKIP;
     const limit = limitStr ? Number(limitStr) : DEFAULT_LIMIT;
-    const namePrep = stationName
-        .toLowerCase()
-        .normalize("NFD")
-        .replace(/\p{Diacritic}/gu, "");
+    const namePrep = prepName(stationName);
     const filtered: Station[] = data.nodes.filter((n) => {
-        const prep = n.fullName
-            .toLowerCase()
-            .normalize("NFD")
-            .replace(/\p{Diacritic}/gu, "");
-        return prep.includes(namePrep);
+        const prepFull = prepName(n.fullName);
+        const prep = prepName(n.name);
+        return prepFull.includes(namePrep) || prep.includes(namePrep);
     });
     const sliced: Station[] = filtered.slice(skip, skip + limit);
     return json({ stations: sliced });
