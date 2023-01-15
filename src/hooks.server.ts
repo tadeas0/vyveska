5cf26f882a7a2bf9a0601b6c240f151409889c64
Tadeas Pala <tadapala@seznam.cz>
2023-01-15 19:56:48 +0100

    log test

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,7 @@ import { locale } from "svelte-i18n";
 
 export const handle: Handle = async ({ event, resolve }) => {
     const lang = event.request.headers.get("accept-language")?.split(",")[0];
+    console.log(`lang is ${lang}`);
     if (lang) {
         locale.set(lang);
     }
