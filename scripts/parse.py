bbcd65092a672a04234c192bf886ac8ca122bf02
Tadeas Pala <tadapala@seznam.cz>
2023-01-16 16:56:51 +0100

    fix data fetching error

--- scripts/parse.py
+++ scripts/parse.py
@@ -5,11 +5,10 @@ res = requests.get("https://data.pid.cz/stops/json/stops.json")
 stops = res.json()
 
 nodes = dict()
-node_ids = set()
 for i in stops["stopGroups"]:
     altKeys = ["name", "fullName", "idosName", "uniqueName"]
     names = set(i[alt] for alt in altKeys)
-    if i["node"] not in node_ids:
+    if i["node"] not in nodes:
         nodes[i["node"]] = {
             "names": [],
             "fullName": i["fullName"],
