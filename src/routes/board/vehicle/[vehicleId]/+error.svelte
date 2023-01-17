2841e2150017efd393ad1945f09371d63c1ef891
Tadeas Pala <tadapala@seznam.cz>
2023-01-17 17:23:54 +0100

    translate vehicle error

--- src/routes/board/vehicle/[vehicleId]/+error.svelte
+++ src/routes/board/vehicle/[vehicleId]/+error.svelte
@@ -1,9 +1,10 @@
 <script>
     import { page } from "$app/stores";
+    import { _ } from "svelte-i18n";
     import Board from "$lib/components/Board.svelte";
 </script>
 
 <Board>
     <h1 slot="title" class="text-3xl text-teal-400">{$page.params.vehicleId}</h1>
-    <h2 slot="items" class="text-xl text-emerald-400">{$page.error?.message}</h2>
+    <h2 slot="items" class="text-xl text-emerald-400">{$_("noVehicleData")}</h2>
 </Board>
